982b55722054c714bc0dce93310863bfaed0378a
Will Albeck <will@albeck.me>
2025-11-29 01:17:57 -0500

    Add idempotent Prisma seed for test users

---
+++ src/lib/auth-client.ts
@@ -0,0 +1,9 @@
+// src/lib/auth-client.ts
+import { createAuthClient } from "better-auth/react";
+
+export const authClient = createAuthClient({
+  baseURL: process.env.NEXT_PUBLIC_APP_URL ?? "http://localhost:3000",
+});
+
+// Optional, for convenience:
+export const { signIn, signOut, useSession } = authClient;
